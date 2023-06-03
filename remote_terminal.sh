4ac949f0a3432a4649469a9a119f73211b6da377
tzukav <79611386+tzukav@users.noreply.github.com>
2023-06-03 17:42:42 +0200

    Update remote_terminal.sh
    Solved not working on port 22 or empty port

--- remote_terminal.sh
+++ remote_terminal.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/bash
-if [[ $1 =~ ^sftp\:\/\/([^\/]*)\@([0-9a-z\.\:]+)\:([0-9]+)(\/.*)? ]]; then
+if [[ $1 =~ ^sftp\:\/\/([^\@]+)\@([0-9\.]+)(\:([0-9]+))?(\/(.*))?$ ]]; then
 user=${BASH_REMATCH[1]}
 host=${BASH_REMATCH[2]}
-port=${BASH_REMATCH[3]}
-path=${BASH_REMATCH[4]}
-password=`(secret-tool lookup server $host user $user port $port) | sed "s/'/\\\\\x27/g"`
+port=${BASH_REMATCH[4]}
+path=${BASH_REMATCH[5]}
+port=${port:-22}
+password=`(secret-tool lookup server $host user $user) | sed "s/'/\\\\\x27/g"`
 if [[ $password != "" ]]; then
 gnome-terminal --tab -- bash -c "export SSHPASS=\$(echo -e '$password'); sshpass -e ssh $user@$host -p $port -t 'cd \"$path\"; bash --login'"
-sleep 2;
-gnome-terminal -- bash -c "history -r; exit"
 fi
 fi
